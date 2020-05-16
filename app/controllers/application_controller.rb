bd660df374751595586919f22f22f4a2e0648dd4
Sharnell Grant <sharnellgrant@gmail.com>
2020-05-16 03:53:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,12 +31,13 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		user = User.find_by(:username => params[:username])
-		 if user && user.authenticate(params[:password])
-		   session[:user_id] = user.id 
-		    redirect "/success"
-		  else
-		    redirect "/failure"
-		  end
+ 
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/success"
+  else
+    redirect "/failure"
+  end
 	end
 
 	get "/success" do #renders a success.erb page, which should be displayed once a user successfully logs in.
