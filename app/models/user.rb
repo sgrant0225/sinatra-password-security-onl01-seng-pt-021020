e92c8cb789426a09aae0115d250f88e8c1b9da92
Sharnell Grant <sharnellgrant@gmail.com>
2020-05-16 02:49:43 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-	
+	has_secure_password
 end
+
+#Macro gives us access to a few new methods. A macro is a method that when called, creates methods for you.
+
+#This method works in conjunction with the gem bcrypt
